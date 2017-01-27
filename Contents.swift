36758a223452e82c5502b0dff9902bd691807ed5
danielCatlett <daniel.catlett42@gmail.com>
2017-01-27 09:32:04 -0600

    Initialize some variables and constants, and play with them

---
+++ Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var var1 = 42
+var var2 = 42.42
+var var3:Float = 42.4242
+
+let con1 = 42
+let con2 = 42.42
+let con3:Float = 42.4242
+
+let exp1:String = "Life, the universe, and everything"
+let exp2:Float = 42.42
+let exp3:Double = 42.4242
+
+let thisWontWork = 0
+//thisWontWork = 1 This doesn't work
+
+let lastOne:Float = 4 //Works just fine
